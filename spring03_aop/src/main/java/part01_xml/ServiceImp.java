c912159f4560a716e7df246c97b8e67011cf7edf
xmmzslla <wppq20011206@gmail.com>
2023-01-19 15:17:35 +0900

    Add Spring

---
+++ spring03_aop/src/main/java/part01_xml/ServiceImp.java
@@ -0,0 +1,60 @@
+package part01_xml;
+
+import java.util.Random;
+
+//핵심로직만을 구현해놓는 클래스 - Target
+public class ServiceImp implements Service{
+	
+	public ServiceImp() {
+		
+	}
+
+	@Override
+	public void prn1() {
+		
+		
+	}
+
+	@Override
+	public void prn1(int x) {
+		
+		
+	}
+
+	@Override
+	public void prn1(Random ran) {
+		
+		
+	}
+
+	@Override
+	public void prn1(int a, int b) {
+		
+		
+	}
+
+	@Override
+	public void prn2() {
+		
+		
+	}
+
+	@Override
+	public String prn3() {
+		
+		return null;
+	}
+
+	@Override
+	public void prn4() {
+		
+		
+	}
+
+	@Override
+	public void prn5() {
+		
+		
+	}
+
+}
